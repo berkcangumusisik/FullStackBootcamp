c13597f43d74ac90df04a7ed2271ea52605d030b
Berkcan Gümüşışık <75336900+berkcangumusisik@users.noreply.github.com>
2024-06-30 09:51:08 +0300

    Day 8 Lesson 1 finished.

---
+++ FullStackBootcamp/MVC.Web/Controllers/FormController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
+using MVC.Web.Models;
+
+namespace MVC.Web.Controllers
+{
+    public class FormController : Controller
+    {
+        [HttpGet] 
+        public IActionResult FormExample()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult FormExample1Post()
+        {
+            //1. Yol 
+            string firstName = Request.Form["firstName"]!;
+            string lastName = Request.Form["lastName"]!;
+            string email = Request.Form["email"]!;
+            //2. Yol 
+            if (Request.Form.TryGetValue("firstName", out StringValues firstNameValues))
+            {
+
+            }
+            //3. Yol 
+            if (Request.Form.ContainsKey("firstName"))
+            {
+                string firstName2 = Request.Form["firstName"]!;
+            }
+
+            
+           
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CreateProduct()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreateProduct(ProductCreateViewModel model)
+        {
+            
+            return View();
+        }
+    }
+}
---
+++ FullStackBootcamp/MVC.Web/Models/ProductCreateViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC.Web.Models
+{
+    public record ProductCreateViewModel
+    {
+
+        [Display(Name = "Ürün Adı")]
+        public string Name { get; set; } =default!;
+        [DataType(DataType.Currency)]
+        [Display(Name = "Ürün Fiyatı")]
+        public decimal Price { get; set; }
+        [Display(Name = "Ürün Açıklaması")]
+        public string? Description { get; set; }
+        [Display(Name = "Stok Sayısı")]
+        public int StockCount { get; set; }
+        [DataType(DataType.ImageUrl)]
+        [Display(Name = "Resim Url")]
+        public string PictureUrl { get; set; } 
+
+        
+    }
+}
